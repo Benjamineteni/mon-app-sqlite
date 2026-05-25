0c5254bdf49ebf8f15c2b9784e96228708ce486f
Eteni elat benjamin <benjamineteni@gmail.com.com>
2026-05-25 14:51:03 +0100

    second commit

--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,5 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-    addUser: (user) => ipcRenderer.invoke('add-user', user),
-    getUsers: () => ipcRenderer.invoke('get-users')
+contextBridge.exposeInMainWorld('db', {
+	listNotes: () => ipcRenderer.invoke('db:listNotes'),
+	addNote: (text) => ipcRenderer.invoke('db:addNote', text),
+	deleteNote: (id) => ipcRenderer.invoke('db:deleteNote', id),
 });
